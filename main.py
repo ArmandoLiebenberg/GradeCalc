fa40dbce2dcfc6f90a772f2d82a356eb8d55cbd1
Armando Liebenberg <armandoliebenberg@protonmail.com>
2020-11-27 03:29:26 +1000

    inital commit of main file, contains some comments about how the program should operate

---
+++ main.py
@@ -0,0 +1,29 @@
+# Program that stores courses and their assessment, lets you update grades,
+# and lets you calculate gpa. Also tells you what grade you need for an assessment
+# / for a final for each desired gpa. 
+# written by Armando Liebenberg
+
+
+# Main
+    # pretty print all courses, their assessments, and their grades
+    # list of course
+    # get all courses
+    # get commands we can run on courses
+
+# Course object
+    # list of assessments in Course
+    # get all assessments
+    # add assessment
+    # remove assessment
+    # change grade of assessment
+    # calculate course grade
+    # calculate marks needed on final
+    # pretty print all asessments and their grades
+
+# Assessment object
+    # grade in assessment
+    # weighting of asessment
+    # print grade
+    # print weighting
+    # get grade
+    # get weighting
